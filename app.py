c2abd436d04dfb0887298c35d3d9958b299e6991
Sivahari0008 <71330599+Sivahari0008@users.noreply.github.com>
2025-08-23 13:45:16 +0530

    app.py
    importing hugging face

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from requests.auth import HTTPBasicAuth
 
 from openai import OpenAI
 from llama_index.core import VectorStoreIndex, Document
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+pip install "llama-index[embed-huggingface]"
+from llama_index.embeddings import HuggingFaceEmbedding
+
 
 # Load environment variables
 load_dotenv()
